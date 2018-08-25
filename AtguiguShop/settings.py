56b28a9748f0f01381c915ea3f34d4fa0ffa52a4
caowenqi <283658013@qq.com>
2018-08-25 10:30:52 +0800

    配置django-redis
    pip install django-redis
    配置全局redis

--- AtguiguShop/settings.py
+++ AtguiguShop/settings.py
@@ -209,3 +209,13 @@ EMAIL_FROM = 'learningandliving@163.com'
 REST_FRAMEWORK_EXTENSIONS = {
     'DEFAULT_CACHE_RESPONSE_TIMEOUT': 10*60
 }
+# django redis
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
